924ba8a9df8fe17b2e80e0b101376be6685c9f32
Mohit Tadhiyal <mohtadhi@publicisgroupe.net>
2018-10-09 01:24:54 +0530

    product code

--- gocart/views/admin/header.php
+++ gocart/views/admin/header.php
@@ -97,7 +97,7 @@ $(document).ready(function(){
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo lang('common_catalog') ?> <b class="caret"></b></a>
                         <ul class="dropdown-menu">
-                            <li><a href="<?php echo $admin_url;?>products/index.php"><?php echo lang('common_products') ?></a></li>
+                            <li><a href="<?php echo $admin_url;?>products/index"><?php echo lang('common_products') ?></a></li>
                             
                         </ul>
                     </li>
